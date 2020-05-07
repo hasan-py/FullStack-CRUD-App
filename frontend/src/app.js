38023e78462ec3397e6b4685e45ef72bba28ff40
hasan-py <hasan29351@gmail.com>
2020-05-08 00:12:08 +0600

    stike unstrike slove

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
 
 
-const TodoList = ({todos}) => {
+const TodoList = ({todos,editHandler,deleteHandler,strikeUnstrike}) => {
 
   	return (
 	    <div className="col-lg-6 col-sm-12 offset-lg-3 offset-sm-1 mb-5">
 			<ul id="listUl" className="list-group">
-
-			{todos.map(todo=> {
-				return(
-					<li key={todo.id} className="list-group-item d-flex justify-content-between align-items-center">
-						<div style={{ cursor:"pointer" }} className="mainTitle">
-						{todo.title}
-						</div>
-						<span>
-							<button className="btn btn-sm mr-1 btn-warning editBtn">Edit</button>
-							<button className="btn btn-sm mr-1 btn-danger deleteBtn">Delete</button>
-						</span>
-					</li>	
-				)
-			})}				
+				{todos.map(todo=> {
+					return(
+						<li key={todo.id} className="list-group-item d-flex justify-content-between align-items-center">
+							<div onClick={()=> strikeUnstrike(todo)} style={{ cursor:"pointer" }} className="mainTitle">
+								{todo.title}
+							</div>
+							<span>
+								<button onClick={()=> editHandler(todo)} className="btn btn-sm mr-1 btn-warning editBtn">Edit</button>
+								<button onClick={()=> deleteHandler(todo.id)} className="btn btn-sm mr-1 btn-danger deleteBtn">Delete</button>							</span>
+						</li>	
+					)
+				})}				
 			</ul>
 		</div>
   	)
@@ -60,14 +58,17 @@ class App extends Component {
 	}
 
 	fetchItem(){
+		console.log('Fetching...')
 		fetch('http://127.0.0.1:8000/api/task-list/')
-			.then(res=> res.json())
-			.then(data=> {
-				this.setState({todo:data})
-		})
+		    .then(response => response.json())
+		    .then(data => 
+		      this.setState({
+		        todo:data
+		      })
+		    )
 	}
 
-	changehandler(e){
+	changeHandler(e){
 		
 		this.setState({
 			activeItem:{
@@ -78,27 +79,70 @@ class App extends Component {
 	}
 	
 	handleSubmit(e){
-		e.preventDefault()
-		console.log(this.state.activeItem)
-		const data =  this.state.activeItem
+	    e.preventDefault()
+	    console.log('ITEM:', this.state.activeItem)
+
+	    var csrftoken = this.getCookie('csrftoken')
+
+	    var url = 'http://127.0.0.1:8000/api/task-create/'
+
+	    if(this.state.editing === true){
+	      url = `http://127.0.0.1:8000/api/task-update/${ this.state.activeItem.id}/`
+	      this.setState({
+	        editing:false
+	      })
+	    }
+
+
+
+	    fetch(url, {
+	      method:'POST',
+	      headers:{
+	        'Content-type':'application/json',
+	        'X-CSRFToken':csrftoken,
+	      },
+	      body:JSON.stringify(this.state.activeItem)
+	    }).then((response)  => {
+	        this.fetchItem()
+	        this.setState({
+	           activeItem:{
+		          id:null, 
+		          title:'',
+		          completed:false,
+		        }
+	        })
+	    }).catch(function(error){
+	      console.log('ERROR:', error)
+	    })
+
+	}
+
+	editHandler = (todo)=> {
+		this.setState({
+	      activeItem:todo,
+	      editing:true,
+	    })
+		console.log(this.state)
+	}
+	
+	deleteHandler = (id)=>{
+		var url = `http://127.0.0.1:8000/api/task-delete/${id}`
 		var csrftoken = this.getCookie('csrftoken');
-		fetch('http://127.0.0.1:8000/api/task-create/',{
-			method:'POST',
+		fetch(url,{
+			method:'DELETE',
 			headers: {
                 'content-type': 'application/json',
                 'X-CSRFToken': csrftoken,
-            },
-			body:JSON.stringify(data)
+            }
 		})
 		.then(res=> {
 			this.fetchItem()
-			this.setState({activeItem:{
-				id:null,
-				title:'',
-				completed:false
-			}})
 		})
-		.catch(err=> console.log(err))
+		.catch(err=> console.log(err))		
+	}
+	
+	strikeUnstrike = (todo)=> {
+		console.log(todo)
 	}
 
 	render() {
@@ -111,7 +155,7 @@ class App extends Component {
 						<div className="container-fluid pt-5 pr-5 pl-5">
 							<div className="col-lg-6 col-sm-12 offset-lg-3 offset-sm-1 mt-5">
 								<form onSubmit={(e)=> this.handleSubmit(e)} id="form" className="form-inline mb-2">
-									<input value={this.state.activeItem.title} onChange={(e)=> this.changehandler(e)} id="title" type="text" className="form-control ml-lg-3 mr-lg-2 ml-sm-0 mr-sm-0 col-lg-9 col-sm-9" id="inlineFormInputName2" placeholder="Type here" />
+									<input value={this.state.activeItem.title} onChange={(e)=> this.changeHandler(e)} id="title" type="text" className="form-control ml-lg-3 mr-lg-2 ml-sm-0 mr-sm-0 col-lg-9 col-sm-9" id="inlineFormInputName2" placeholder="Type here" />
 									<button className="btn btn-info btn-sm col-lg-2 col-sm-3 ">Submit</button>
 								</form>
 							</div>
@@ -119,7 +163,7 @@ class App extends Component {
 					</div>
 					<div className="buildList container">
 						<div className="container-fluid pr-5 pl-5">
-							<TodoList todos={this.state.todo} />
+							<TodoList todos={this.state.todo} editHandler={this.editHandler} deleteHandler={this.deleteHandler} strikeUnstrike={this.strikeUnstrike} />
 						</div>
 					</div>
 				</div>
